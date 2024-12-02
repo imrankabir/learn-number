3dd10c3cab9a444a29e77ee117f091b5f3893b7b
Imran Kabir <imran.kabir.k@gmail.com>
2024-12-02 07:44:47 +0500

    learn-number auto commit on 2024-12-02 07:44:47

--- script.js
+++ script.js
@@ -187,6 +187,11 @@ document.addEventListener('keydown', e => {
         canvas.height = 120;
         box.appendChild(canvas);
 
+        canvas.addEventListener('pointerdown', e => startPosition(canvas, e));
+        canvas.addEventListener('pointermove', e => draw(canvas, e));
+        canvas.addEventListener('pointerup', e => endPosition(canvas, e));
+        canvas.addEventListener('pointerout', e => endPosition(canvas, e));
+
         canvas.addEventListener('mousedown', e => startPosition(canvas, e));
         canvas.addEventListener('mouseup', e => endPosition(canvas, e));
         canvas.addEventListener('mouseleave', e => endPosition(canvas, e));
